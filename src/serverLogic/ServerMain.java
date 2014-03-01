5e7d4bd24903526dca6e46c7b22579f54f9f9a35
Draymire <zachstoner@gmail.com>
2014-03-01 13:13:29 -0500

    Pseudo Code in Mains
    Added some pseudo code for the generic behaviour of the main

--- src/serverLogic/ServerMain.java
+++ src/serverLogic/ServerMain.java
@@ -9,6 +9,18 @@ public class ServerMain {
 		// TODO Auto-generated method stub
 		Logger log = new Logger(); 
 		GameBoard game = new GameBoard();
+		
+		//	Begin listening
+		//	Loop
+		//		Establish Connection
+		//		Populate Gameboard
+		// 		Loop
+		//			Send Gameboard Status
+		//			Check for and Execute Commands
+		// 			Update Gameboard
+		//			Check Player Life Status
+		// 		Kill Connection
+		// 		Wait for new connection
 
 	}
 
