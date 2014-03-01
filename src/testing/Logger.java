f2aee704c423fe18fb1a1831b296c6a15f47045d
tmjohari <tmjohari@gmail.com>
2014-03-01 13:38:37 -0500

    Simple logger

--- src/testing/Logger.java
+++ src/testing/Logger.java
@@ -1,5 +1,26 @@
 package testing;
 
-public class Logger {
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
+public class Logger {
+	private String log;
+	PrintWriter out;
+	public Logger(){
+		
+		try {
+			out = new PrintWriter("Log.txt");
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public void update(String log){
+		this.log+=log;
+		log+= "\n";
+	}
+	public void toFile(){
+		out.println(log);
+		this.log = "";
+	}
 }
