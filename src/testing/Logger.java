35c6e3d10d6f0988a22106f91a49cf86e82f6106
tmjohari <tmjohari@gmail.com>
2014-03-01 14:15:34 -0500

    Added print board to file

--- src/testing/Logger.java
+++ src/testing/Logger.java
@@ -4,19 +4,30 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
 public class Logger {
-	//private String log;
+	
 	PrintWriter out;
 	public Logger(){
 		
 		try {
 			out = new PrintWriter("Log.txt");
-		} catch (FileNotFoundException e) {
+		} 
+		catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	public void update(String log){
+	//Every command player made
+	public void commandLog(String log){
 		out.println(log);
-		//log+= "\n";
+	}
+	
+	//Board update
+	public void boardLog(char[][] board){
+		for(int row=0;row < board.length;row++){
+			for(int col=0;col < board[0].length;col++){
+				out.print(board[row][col]);
+			}
+			out.println();
+		}
 	}
 	
 }
