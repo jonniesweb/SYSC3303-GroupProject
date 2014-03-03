47ec1063bcefd8fd799f268b4ec60d3edf547a45
Jon Simpson <jonniesweb@gmail.com>
2014-03-02 19:54:15 -0500

    organized imports and formatted code

--- src/serverLogic/NetworkManager.java
+++ src/serverLogic/NetworkManager.java
@@ -1,32 +1,34 @@
 package serverLogic;
-import java.util.concurrent.*;
 
-import Networking.*;
+import java.util.concurrent.Semaphore;
+
+import Networking.Message;
+import Networking.Network;
 
 //TODO: construct logic Manager with playerlist 
 public class NetworkManager {
 	private Semaphore inboxLock;
-	private Network   net;
+	private Network net;
 	private LogicManager logic;
 	private String command;
-	
-	public NetworkManager(){
-		//logic = new LogicManger();
-		inboxLock =  new Semaphore(0);
-		net = new Network(Network.SEVER_PORT_NO,inboxLock);
+
+	public NetworkManager() {
+		// logic = new LogicManger();
+		inboxLock = new Semaphore(0);
+		net = new Network(Network.SEVER_PORT_NO, inboxLock);
 		new Thread(net).start();
 		new Thread(logic).start();
-		
+
 	}
-	private Message readInbox(){
-		try{
+
+	private Message readInbox() {
+		try {
 			inboxLock.acquire();
-		}catch(InterruptedException e){
+		} catch (InterruptedException e) {
 			System.out.println("Thread Interrupted returning empty message");
 			return null;
 		}
 		return net.getMessage();
 	}
-	
-	
+
 }
