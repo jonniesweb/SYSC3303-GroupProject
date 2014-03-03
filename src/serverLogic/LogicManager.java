a8d267fcb07180d26e741f29ce499ecd23519fe0
Draymire <zachstoner@gmail.com>
2014-03-03 17:29:25 -0500

    LogicManager/NetworkManager
    removed unused variables and method calls

--- src/serverLogic/LogicManager.java
+++ src/serverLogic/LogicManager.java
@@ -35,8 +35,6 @@ public class LogicManager implements Runnable {
 	
 	private BlockingQueue<Message> commandQueue = new LinkedBlockingQueue<Message>();
 	
-	private List<Player> playerList;
-	
 	private GameBoard board;
 	
 	private NetworkManager networkManager;
