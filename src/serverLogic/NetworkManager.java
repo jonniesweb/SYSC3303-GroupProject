e0140bf776463fd525f8447c1f7572ac5b680928
kareem1234 <kareem.lewis79@gmail.com>
2014-03-02 19:16:09 -0500

    added Networking manager ... i think

--- src/serverLogic/NetworkManager.java
+++ src/serverLogic/NetworkManager.java
@@ -1,5 +1,32 @@
 package serverLogic;
+import java.util.concurrent.*;
 
+import Networking.*;
+
+//TODO: construct logic Manager with playerlist 
 public class NetworkManager {
-
+	private Semaphore inboxLock;
+	private Network   net;
+	private LogicManager logic;
+	private String command;
+	
+	public NetworkManager(){
+		//logic = new LogicManger();
+		inboxLock =  new Semaphore(0);
+		net = new Network(Network.SEVER_PORT_NO,inboxLock);
+		new Thread(net).start();
+		new Thread(logic).start();
+		
+	}
+	private Message readInbox(){
+		try{
+			inboxLock.acquire();
+		}catch(InterruptedException e){
+			System.out.println("Thread Interrupted returning empty message");
+			return null;
+		}
+		return net.getMessage();
+	}
+	
+	
 }
