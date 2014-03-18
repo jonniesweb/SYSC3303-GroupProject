b0ef55979d92b8b1bee999969c1510e1888ff782
Jon Simpson <jonniesweb@gmail.com>
2014-03-18 15:01:48 -0400

    Created a superclass of UserMessage called Message

---
+++ src/Networking/Message.java
@@ -0,0 +1,19 @@
+package Networking;
+
+public class Message {
+
+	public String message;
+
+	public Message(String message) {
+		this.message = message;
+	}
+
+	/**
+	 * Returns a string representation of the message.
+	 * @return
+	 */
+	public String getData() {
+		return message;
+	}
+
+}
--- src/Networking/UserMessage.java
+++ src/Networking/UserMessage.java
@@ -3,24 +3,26 @@ package Networking;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 
-public class UserMessage {
+public class UserMessage extends Message {
 	public DatagramPacket datagram;
-	public String message;
 	public InetAddress ip;
 	public int port;
 	public long time;
 
 	public UserMessage(DatagramPacket p) {
+		super(new String(p.getData()).trim());
 		datagram = p;
 		ip = p.getAddress();
 		port = p.getPort();
-		message = new String(datagram.getData()).trim();
 	}
 	
 	public UserMessage(String message, String hostName, int port, long time) {
+		super(message);
 		this.time = time;
 		this.port = port;
 		this.message = message;
+		
+		// TODO: Remove dependency on having localhost as the server
 		hostName = "127.0.0.1";
 		try {
 			ip = InetAddress.getByName(hostName);
@@ -28,14 +30,5 @@ public class UserMessage {
 			System.out.println(e);
 		}
 		datagram = new DatagramPacket(message.getBytes(), message.getBytes().length, ip, port);
-	}
-	
-	/**
-	 * Returns a String of the data of the message. Calls the trim() method on
-	 * the string to remove extraneous whitespace.
-	 * @return
-	 */
-	public String getData() {
-		return message;
 	}
 }
